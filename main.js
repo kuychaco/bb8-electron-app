36024af9d5c584fa87f886bf5cfd72040c157689
Katrina Uychaco <kuychaco@github.com>
2016-08-19 14:53:10 -0700

    Create tray icon and hide dock icon

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const path = require('path')
 
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Tray} = require('electron')
 
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow()
+  createTray()
+})
+
+let tray
+const createTray = () => {
+  tray = new Tray(path.join(__dirname, 'images', 'icon.png'))
+}
 
 let mainWindow
 function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
   mainWindow.loadURL(`file://${path.join(__dirname, 'index.html')}`)
 }
+
+app.dock.hide()
