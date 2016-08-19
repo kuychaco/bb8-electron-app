6e35b33280befc9861549e6aa024341c0e2ac331
Katrina Uychaco <kuychaco@github.com>
2016-08-19 14:55:29 -0700

    Toggle and position window

--- main.js
+++ main.js
@@ -2,19 +2,46 @@ const path = require('path')
 
 const {app, BrowserWindow, Tray} = require('electron')
 
+const Positioner = require('electron-positioner')
+
 app.on('ready', () => {
-  createWindow()
   createTray()
 })
 
 let tray
 const createTray = () => {
   tray = new Tray(path.join(__dirname, 'images', 'icon.png'))
+  tray.on('click', (event, trayBounds) => toggleWindow(trayBounds))
+}
+
+const toggleWindow = (trayBounds) => {
+  if (!mainWindow) createWindow()
+  if (mainWindow.isVisible()) {
+    mainWindow.hide()
+  } else {
+    showWindow(trayBounds)
+  }
+}
+
+const showWindow = (trayBounds) => {
+  const positioner = new Positioner(mainWindow)
+  const position = positioner.calculate('trayCenter', trayBounds)
+  mainWindow.setPosition(position.x, position.y, false)
+  mainWindow.show()
+  mainWindow.focus()
 }
 
 let mainWindow
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 525,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false
+  })
+  mainWindow.on('blur', () => mainWindow.hide())
   mainWindow.loadURL(`file://${path.join(__dirname, 'index.html')}`)
 }
 
